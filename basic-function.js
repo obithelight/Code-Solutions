73b8d9e146ac2cae5cd77b3e4b95e19ac68e278f
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-07 23:46:19 +0800

    initial commit

---
+++ basic-function.js
@@ -0,0 +1,15 @@
+/*
+Fix the function
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+*/
+
+//Solution 1
+ function addFive(num) {
+   let total = num + 5
+   return total
+}
+
+//Solution 2
+const addFive = num => num + 5
