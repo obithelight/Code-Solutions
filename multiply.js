1624f52dea88b852802ad79b730bb9c766777b39
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-08 02:40:52 +0800

    initial commit

---
+++ multiply.js
@@ -0,0 +1,15 @@
+/*
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+*/
+
+//Solution 1
+const simpleMultiplication = number => number % 2 == 0 ? number * 8 : number * 9
+
+//Solution 2
+function simpleMultiplication(number) {
+    if(number % 2 == 0){
+      return number * 8
+    }else{
+      return number * 9
+    }
+}
