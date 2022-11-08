acfc938f5995a236c4bfd85ad9f4e7246ca19b21
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-09 04:29:21 +0800

    initial commit

---
+++ apple-turnover.js
@@ -0,0 +1,6 @@
+/*
+Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+*/
+
+//Solution 
+const apple = x => Math.pow(x, 2) > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
